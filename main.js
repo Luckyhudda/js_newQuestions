74d6c9eac8849387d6ed1ca6bcf737a5925c97a7
Lucky Hudda <113304073+Luckyhudda@users.noreply.github.com>
2023-06-12 18:54:11 +0530

    new ques. added

--- main.js
+++ main.js
@@ -620,4 +620,25 @@ console.log(plusOne([9,9,8]));
 
 
 
+// 116  Given an integer n, return true if n has exactly three positive divisors. Otherwise, return false.
+//An integer m is a divisor of n if there exists an integer k such that n = k * m.
+
+let n = 12;
+
+var isThree = function (n) {
+   let count = 0;
+   for(let i = 1;i<=n ;i++){
+      if(n % i ==0){
+         count++
+         
+      }
+   }
+   if(count == 3){
+      return true;
+   }
+   return false;
+};
+console.log(isThree(n));
+
+
 
